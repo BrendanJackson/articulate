ffc59e733fcad8ec484aca5148ad8619932c50cb
BrendanJackson <bajackson859@gmail.com>
2016-07-21 11:49:41 -0400

    initial commit

---
+++ views/footer.php
@@ -0,0 +1,82 @@
+<!-- contact begins -->
+<section id="page-contact" class="page-contact">
+    <div class="container">
+        <header class="section-header">
+            <h2 class="section-title"><span>Contact</span></h2>
+            <div class="spacer"></div>
+            <p class="section-subtitle">Lorem ipsum dolor sit amet, id iusto oportere mel. </p>
+        </header>
+        <div class="row">
+            <div class="col-sm-5 contact-info">
+                <h3>Contact Info</h3>
+
+                <?php
+                if (defined('config::site_address')){
+                    if(!empty(\config::site_address)){ /*site_address_2 site_city site_state*/
+                        echo "<p><i class='fa fa-map-marker'></i> " . \config::site_address . " " .\config::site_address_2 . "</p>";
+                        echo "<p>" . "&nbsp; &nbsp; " . \config::site_city . " " . \config::site_state . " " . \config::site_zip . " " . "</p>";
+                    }
+                }
+                ?>
+                <?php
+                if (defined('config::site_phone')){
+                    if(!empty(config::site_phone)){
+                        echo "<p><i class='fa fa-phone'></i> " . config::site_phone. "</p>";
+                    }
+                }
+                ?>
+                <?php
+                if (defined('config::site_email')){
+                    if(!empty(config::site_email)){
+                        echo "<p><i class='fa fa-envelope-o'></i> " . config::site_email . "</p>";
+                    }
+                }
+                ?>
+            </div>
+
+            <div class="col-sm-7">
+                <h3>Get in Touch</h3>
+                <form  class="form-horizontal" id="contact-form" action="MAILTO:<?=\config::site_email?>" method="post" enctype="text/plain">
+                    <div class="control-group">
+                        <label class="control-label" for="name">Name</label>
+                        <div class="controls">
+                            <input type="text" name="name" id="name" placeholder="Your name" class="form-control input-lg ">
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label" for="email">Email Address</label>
+                        <div class="controls">
+                            <input type="text" name="email" id="email" placeholder="Your email address" class="form-control input-lg">
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label" for="message">Message</label>
+                        <div class="controls">
+                            <textarea name="message" id="message" rows="8" class="form-control input-lg"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-actions">
+                        <button type="submit" class="btn btn-default btn-lg btn-block">Submit Message</button>
+                    </div>
+                </form><!-- End contact-form -->
+            </div>
+        </div><!-- End row -->
+
+    </div>
+</section> <!-- contact ends -->
+
+
+<script src="<?=$assetfolder?>js/jquery-1.10.2.min.js"></script>
+<script src="<?=$assetfolder?>js/bootstrap.min.js"></script>
+<script src="<?=$assetfolder?>js/jquery.scrollTo.js"></script>
+<script src="<?=$assetfolder?>js/jquery.nav.js"></script>
+<script src="<?=$assetfolder?>js/jquery.sticky.js"></script>
+<script src="<?=$assetfolder?>js/jquery.easypiechart.min.js"></script>
+<script src="<?=$assetfolder?>js/jquery.vegas.min.js"></script>
+<script src="<?=$assetfolder?>js/jquery.isotope.min.js"></script>
+<script src="<?=$assetfolder?>js/jquery.magnific-popup.min.js"></script>
+<script src="<?=$assetfolder?>js/jquery.validate.js"></script>
+<script src="<?=$assetfolder?>js/waypoints.min.js"></script>
+<script src="<?=$assetfolder?>js/main.js"></script>
+</body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,110 @@
+<?php
+$assetfolder = "/themes/articulate/assets/";
+$logo = "/assets/files/id/" . \config::site_logo;
+$logo_alternate = "/assets/files/id/" . \config::site_logo_alternative;
+$modules = \sa\application\app::get()->getModules();
+$module_list = array_column($modules, 'module');
+$search = in_array('search', $module_list);
+$catalog = in_array('catalog', $module_list);
+$member = in_array('member', $module_list);
+
+?>
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Articulate</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="">
+
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="<?=$assetfolder?>css/bootstrap.min.css">
+    <!-- Fonts from Font Awsome -->
+    <link rel="stylesheet" href="<?=$assetfolder?>css/font-awesome.min.css">
+    <!-- Magnific popup -->
+    <link rel="stylesheet" href="<?=$assetfolder?>css/magnific-popup.css">
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="<?=$assetfolder?>css/main.css">
+
+    <!-- Color styles -->
+    <link rel="stylesheet" href="<?=$assetfolder?>css/colors/blue.css">
+    <!-- <link rel="stylesheet" href="<?=$assetfolder?>css/colors/yellow.css">-->
+    <!-- <link rel="stylesheet" href="<?=$assetfolder?>css/colors/red.css">-->
+    <!--  <link rel="stylesheet" href="<?=$assetfolder?>css/colors/purple.css">-->
+    <!--  <link rel="stylesheet" href="<?=$assetfolder?>css/colors/orange.css">-->
+    <!--  <link rel="stylesheet" href="<?=$assetfolder?>css/colors/green.css">-->
+
+    <!-- Feature detection -->
+    <script src="<?=$assetfolder?>js/modernizr-2.6.2.min.js"></script>
+    <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900,300italic,400italic,600italic,700italic,900italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Oxygen:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="<?=$assetfolder?>js/plugins/html5shiv.js"></script>
+    <script src="<?=$assetfolder?>js/plugins/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav" id="main-menu">
+                <?php
+                $navigation = \sa\application\modRequest::request('site.navigation', $data = array('level' => 2));
+                foreach ($navigation['navigation']['page_editor'] as $menuItem){
+                    ?>
+
+                    <li class="dropdown  <?= ($menuItem['has_sub_pages'] == 1) ? 'has-children' : '' ?>">
+                        <a  href="<?=($menuItem['route'] != '/') ? '/'.$menuItem['route']:'/'?>" class="dropdown-toggle" data-toggle="dropdown">
+                            <span><?= $menuItem['title'] ?></span>
+                            <?= ($menuItem['has_sub_pages'] == 1) ? '<span class="sf-sub-indicator"><i class="fa fa-angle-down"></i></span>' : '' ;?>
+                        </a>
+
+                        <?php if($menuItem['has_sub_pages'] == 1){
+                            echo '<ul class="dropdown-menu">';
+
+                            foreach ($menuItem['sub_pages'] as $pages) {
+                                echo "<li class=''><a href='" . $pages['title'] . " '> ". $pages['title'] . "</a></li>" ;
+                            }
+                            echo "</ul>" ;
+                        } ?>
+                    </li>
+                    <?php
+                } ?>
+                <!-- TODO: ADD SEARCH AND CART? -->
+                <?php/*
+                if(!empty($search)){
+                    echo
+                    "<li class='dropdown'>
+                        <form role='search' class='search-box' method='GET' action='/search'>
+                            <i class='search fa fa-search search-btn'></i> <input type='text' class='' placeholder='Search' name='q'>
+                            <button class='btn-u' type='button'>Go</button>
+                        </form>
+                    </li>";
+                }*/?>
+
+            </ul>
+
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
